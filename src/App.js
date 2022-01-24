5f91ebb7e6dddbf270ac4af8fc17bd36d771561c
saad710 <ifrathasnat2@gmail.com>
2022-01-25 00:27:45 +0600

    year-issue-fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 
-function App() {
+import moment from 'moment';
 
-let events = [
+
+const App = () => {
+  
+  let events = [
     { startDate: '10-01-2022', endDate: '20-01-2022', id: '001' },
     { startDate: '10-01-2022', endDate: '14-01-2022', id: '008' },
     { startDate: '21-01-2022', endDate: '25-01-2022', id: '002' },
@@ -12,67 +15,44 @@ let events = [
     { startDate: '18-01-2022', endDate: '18-01-2022', id: '009' },
     { startDate: '25-01-2022', endDate: '30-01-2022', id: '006' },
     { startDate: '26-01-2022', endDate: '30-01-2022', id: '007' },
-    { startDate: '15-01-2022', endDate: '16-01-2022', id: '102' },
+    { startDate: '15-01-2022', endDate: '16-01-2024', id: '102' },
   ];
-  
- //sorting-events
- let mainData = events?.sort(function (current, next) {
-  let currentStart = new Date(current?.startDate),
-    nextStart = new Date(next?.startDate)
-  let currentEnd = new Date(current?.endDate),
-    nextEnd = new Date(next?.endDate)
-  if (currentStart === nextStart) {
-    if (currentEnd < nextEnd) {
-      return 1
-    }
-    if (currentEnd > nextEnd) {
-      return -1
-    }
-  }
-  if (currentStart > nextStart) {
-    return -1
-  }
-  return 1
-})
+  // console.log(moment('16-01-2022', "DD-MM-YYYY").isBefore(moment('17-01-2023', "DD-MM-YYYY")));
+
+  //sorting-events
+  let mainData = events?.sort((current, next) => next.startDate > current.startDate ? -1 : 1)
   console.log(mainData)
 //find-group-event
-let putEvent = []
-const groupEvent = (tempArray) => {
-  console.log(tempArray)
-  let firstObj = tempArray[0]
-  let slotEvent = [firstObj]
-  let filteredItem = [];
-  tempArray?.forEach(temp => {
-    if(firstObj.endDate < temp.startDate){
-      firstObj = temp
-      slotEvent.push(temp)
+  let putEvent = []
+  const groupEvent = (tempArray) => {
+    let firstObj = tempArray[0]
+    let slotEvent = [firstObj]
+    let filteredItem = [];
+    tempArray?.forEach(temp => {
+      if(moment(firstObj.endDate, "DD-MM-YYYY").isBefore(moment(temp.startDate, "DD-MM-YYYY"))){
+        firstObj = temp
+        slotEvent.push(temp)
+      }
+      filteredItem = tempArray.filter((ind) => {
+      return ind.id !== firstObj.id
+      });
+        tempArray = filteredItem
+      })
+      putEvent.push(slotEvent)
+      if(tempArray.length !== 0){
+        groupEvent(tempArray);
+      }
+      return putEvent;
     }
-    filteredItem = tempArray.filter((ind) => {
-    return ind.id !== firstObj.id
-    });
-      tempArray = filteredItem
-    })
-    putEvent.push(slotEvent)
-    if(tempArray.length !== 0){
-      groupEvent(tempArray);
-    }
-    return putEvent;
-  }
 //final-result
 const result = groupEvent(mainData)
 console.log(result)
 
   return (
-    <div className="App">
-        {
-          result?.map((date,index) => (
-           date.map(dates => (
-            <h2 key={dates.id}>{`${dates.startDate}-${dates.endDate}`}</h2>
-           ))
-          ))
-        }
+    <div>
+       <h1>hello</h1>
     </div>
   );
-}
+};
 
 export default App;
