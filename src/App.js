2fbc04675e6dd96ef2e98bf22e75326e4e0912f8
Md. Ifrat Hasnat <ifrathasnat2@gmail.com>
2022-01-24 13:03:48 +0600

    overlapping-dates-fixed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+let events = [
+    { startDate: '10-01-2022', endDate: '20-01-2022', id: '001' },
+    { startDate: '10-01-2022', endDate: '14-01-2022', id: '008' },
+    { startDate: '21-01-2022', endDate: '25-01-2022', id: '002' },
+    { startDate: '10-01-2022', endDate: '15-01-2022', id: '004' },
+    { startDate: '17-01-2022', endDate: '20-01-2022', id: '005' },
+    { startDate: '20-01-2022', endDate: '29-01-2022', id: '101' },
+    { startDate: '18-01-2022', endDate: '18-01-2022', id: '009' },
+    { startDate: '25-01-2022', endDate: '30-01-2022', id: '006' },
+    { startDate: '26-01-2022', endDate: '30-01-2022', id: '007' },
+    { startDate: '15-01-2022', endDate: '16-01-2022', id: '102' },
+  ];
+
+  //sorting-events
+  let mainData = events?.sort(function (current, next) {
+    let currentStart = current?.startDate,
+      nextStart = next?.startDate
+    let currentEnd = current?.endDate,
+      nextEnd = next?.endDate
+    if (currentStart === nextStart) {
+      if (currentEnd < nextEnd) {
+        return -1
+      }
+      if (currentEnd > nextEnd) {
+        return 1
+      }
+    }
+    if (currentStart > nextStart) {
+      return 1
+    }
+    return -1
+  })
+  console.log(mainData)
+//find-group-event
+let putEvent = []
+const groupEvent = (tempArray) => {
+  console.log(tempArray)
+  let firstObj = tempArray[0]
+  let slotEvent = [firstObj]
+  let filteredItem = [];
+  tempArray?.forEach(temp => {
+    if(firstObj.endDate < temp.startDate){
+      firstObj = temp
+      slotEvent.push(temp)
+    }
+    filteredItem = tempArray.filter((ind) => {
+    return ind.id !== firstObj.id
+    });
+      tempArray = filteredItem
+    })
+    putEvent.push(slotEvent)
+    if(tempArray.length !== 0){
+      groupEvent(tempArray);
+    }
+    return putEvent;
+  }
+//final-result
+const result = groupEvent(mainData)
+console.log(result)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {
+          result?.map((date,index) => (
+           date.map(dates => (
+            <h2 key={dates.id}>{`${dates.startDate}-${dates.endDate}`}</h2>
+           ))
+          ))
+        }
     </div>
   );
 }
